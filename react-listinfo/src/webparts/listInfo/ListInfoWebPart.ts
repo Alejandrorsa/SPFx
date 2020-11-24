b4c8bd1bb25244cd7b4139ddb4fa11a7402b542f
DESKTOP-GG41B21\ALEJANDRO <alejandrubio25@gmail.com>
2020-11-24 19:23:52 +0100

    custom property pane

---
+++ react-listinfo/src/webparts/listInfo/ListInfoWebPart.ts
@@ -0,0 +1,100 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './ListInfoWebPart.module.scss';
+import * as strings from 'ListInfoWebPartStrings';
+
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
+
+
+export interface IListInfoWebPartProps {
+  description: string;
+}
+export interface IListInfoWebPartProps {
+  description: string;
+  listName: string;
+}
+export default class ListInfoWebPart extends BaseClientSideWebPart<IListInfoWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IListItemsProps> = React.createElement(ListItems, {
+      listName: this.properties.listName
+    });
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  private validateDescription(value: string): string {
+    if (value === null ||
+      value.length === 0) {
+      return 'Provide a description';
+    }
+
+    if (value.length > 40) {
+      return 'Description should not be longer than 40 characters';
+    }
+
+    return '';
+  }
+
+  private async validateListName(value: string): Promise<string> {
+    if (value === null || value.length === 0) {
+      return "Provide the list name";
+    }
+
+    try {
+      let response = await this.context.spHttpClient.get(
+        this.context.pageContext.web.absoluteUrl +
+          `/_api/web/lists/getByTitle('${escape(value)}')?$select=Id`,
+        SPHttpClient.configurations.v1
+      );
+
+      if (response.ok) {
+        return "";
+      } else if (response.status === 404) {
+        return `List '${escape(value)}' doesn't exist in the current site`;
+      } else {
+        return `Error: ${response.statusText}. Please try again`;
+      }
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel,
+                  onGetErrorMessage: this.validateDescription.bind(this)
+                }),
+                PropertyPaneTextField('listName',{
+                  label: strings.DescriptionFieldLabel,
+                  onGetErrorMessage: this.validateListName.bind(this)
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
